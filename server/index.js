6a8fc631e32243d20a6a1f16e8144145af094e1e
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-20 18:47:57 +0530

    Added user model

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
-const express = require("express")
-const socketio = require("socket.io")
-const http = require("http")
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 
-const router = require("./router.js")
+const router = require("./router.js");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/chat")
+  .then(() => console.log("Mongoose running"))
+  .catch(() => console.log("Mongoose Errpr"));
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 io.on("connection", (socket) => {
-    console.log("A new user just joined")
-    socket.on("disconnect", () => {
-        console.log("User Left")
-    })
-})
+  console.log("A new user just joined");
+  socket.on("disconnect", () => {
+    console.log("User Left");
+  });
+});
 
-app.use(router)
+app.use(router);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Server running on ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
